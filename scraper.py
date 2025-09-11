f014c1621b6a75133b6f59ec429f6cd460d212ab
Scott Field <fieldscott1@gmail.com>
2025-09-11 15:49:47 -0400

    Fixed link bug, organized data into dictionaries, deleted series.py

--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ Purpose:
 Using selenium scrape data from a given movie/tv show title
 then output that data to the user
 '''
-from series import Series
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -45,18 +44,64 @@ titlesList = resultsBox.find_element(
 titles = titlesList.find_elements(
     By.XPATH,"./li"
 )
-#Grab Links from titlesList
+#Grab Links from titlesList (each link is within two div tags)
 links = titlesList.find_elements(
-    By.TAG_NAME,"a"
+    By.XPATH, ".//div/div/a"
 )
-#The number of Links is the number of titles
-numTitles = len(links)
 
-inner = 0
+#Grab number of titles
+numTitles = len(titles)
+
+#Organize Text From Obtained Data
+showList = []
 for index in range(0,numTitles,1):
-    show = Series()
-    show.title = titles[index].text
-    print(f"{index}:{show.title}")
+    #Store data obtained from show
+    show = {}
+    #Add title to show
+    currentTitle = titles[index]
+    currentLink = links[index]
+    responseText = currentLink.text
+    #Add other data to show
+    ulList = currentTitle.find_elements(By.TAG_NAME,"ul")
+    #Adds data from show to dictionary
+    for counter in range(0,len(ulList),1):
+        ul = ulList[counter]
+        liList = ul.find_elements(By.TAG_NAME,"li")
+        match counter:
+            #if first row
+            case 0:
+                match len(liList):
+                    case 1:
+                        show["title"] = responseText
+                        show["type"]    = liList[0].text
+                        counter+=1 #nothing in second row
+                    case 2:
+                        show["date"]    = liList[0].text
+                        show["type"]    = liList[1].text
+                    case 3:
+                        show["date"]    = liList[0].text
+                        show["episode"] = liList[1].text
+                        show["epType"]  = liList[2].text
+            #if second row
+            case 1:
+                match len(liList):
+                    case 1:
+                        show["title"]   = responseText
+                        show["actors"]  = liList[0].text
+                    case 2:
+                        show["epTitle"] = responseText
+                        show["title"]   = liList[0].text
+                        show["type"]    = liList[1].text
+
+    #add the link to the show
+    #show["link"] = None
+    #add all shows to the list
+    showList.append(show)
+    #print show data
+    print(f"{index}:\n{'='*50}")
+    for key,value in show.items():
+        print(f"\t{key}\t:\t{value}")
+    print(f"{'='*50}")
 
 
 #User Enters Selected Title Index
--- series.py
+++
@@ -1,7 +0,0 @@
-class Series:
-    def __init__(self):
-        self.title = None #Always contained in <a> text
-        self.dateStr = None #Always in 2nd presentation line 1st <li>
-        self.epsTypeStr = None #If 2nd presentation line contains exactly 2 <li>
-        self.actStr = None #If 3rd presentation line contains 1<li>
-        self.text = None #Contained in next page
